c12a38c7560f6e3075cbcdde1657d6aa501fabc1
jaketgr <77414238+jaketgr@users.noreply.github.com>
2022-02-28 22:31:24 -0600

    Add files via upload

---
+++ Project 1/JLetter.java
@@ -0,0 +1,26 @@
+ /** 
+ * Prints out the letter J using the word JAVA.
+ *
+ * @author Jacob Knight
+ * @version 1/21/21
+ */
+public class JLetter {
+
+   /**
+    * Prints a big J to std output.
+    *
+    * @param args Command line arguments (not used).
+    */
+   public static void main(String[] args) {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("  JAVAJA");
+   }
+}
---
+++ Project 1/MyLifeGoals.java
@@ -0,0 +1,27 @@
+ /** 
+ * Prints out my short term, medium term and long term life goals.
+ *
+ * @author Jacob Knight
+ * @version 1/21/21
+ */
+public class MyLifeGoals {
+
+   /**
+    * Prints my life goals to std output.
+    *
+    * @param args Command line arguments (not used).
+    */
+   public static void main(String[] args) {
+      System.out.println("Jacob Knight");
+      System.out.println("");
+      System.out.println("My short-term goals are to finish this semester "
+         + "with above a 3.0 GPA to keep my scholarships "
+         + "and start interning during the summer.");
+      System.out.println("My medium-term goals are to graduate college with a "
+         + "degree in Computer Science and a minor in Management while "
+         + "keeping my scholarships.");
+      System.out.println("My long-term goals are to start a career in "
+         + "Cybersecurity while moving back to my hometown, getting "
+         + "a house, and starting a family.");
+   }
+}
